ad0285a6b71bcdd91b30245953aef7523988eb72
filipelabs <filipelabs@gmail.com>
2024-09-15 12:18:10 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -33,8 +33,11 @@ let currentImgIndex = 0;
 
 // Função para mostrar a imagem ativa com transição
 function showImage(index) {
-  carouselImgs.forEach(img => img.classList.remove('active'));
-  carouselImgs[index].classList.add('active');
+  // Esconde todas as imagens antes de mostrar a ativa
+  carouselImgs.forEach(img => img.style.display = 'none');
+
+  // Mostra apenas a imagem ativa
+  carouselImgs[index].style.display = 'block';
 }
 
 // Event listeners para os botões
